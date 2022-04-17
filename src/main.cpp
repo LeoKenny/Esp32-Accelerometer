2538d7d28059664f44feb7ffd02f9c8fc4ae01e4
Leo Kenny <leo.treicheldacunha@gmail.com>
2022-04-17 00:00:25 -0300

    MPU configuration and data acquisition done directly using Wire.h library

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
-
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 #define DEBUG 1
 #define TIMER_PRESCALER 80
 #define TIMER_COUNTER_UP true
-#define TIMER_COUNTER_DELAY 1000000  // (Clock/Prescaler) counted units
+#define TIMER_COUNTER_DELAY 1000000   // (Clock/Prescaler) counted units
 #define TIMER_AUTO_RELOAD true
+#define MPU_ADDR (uint16_t)0x68
+#define CONTINUE_COMM (bool)true
+#define I2C_CLOCK 400000L
+#define PWR_MGMT_1 0x6B             // Power management register 1
+#define PWR_MGMT_2 0x6C             // Power management register 2
+#define SMPRT_DIV 0x19              // Sample Rate divider register
+#define CONFIG 0x1A                 // Config register
+#define ACCEL_CONFIG 0x1C           // Accelerometer Configuration Register
+#define FIFO_EN 0x23                // FIFO Enable register
+#define INT_ENABLE 0x38             // INT Enable register
+#define INT_STATUS 0x3A             // INT Status register
+#define ACCEL_ZOUT_H 0x3F           // High bits from last read accelerometer value
+#define ACCEL_ZOUT_L 0x40           // Low bits from last read accelerometer value
+#define USER_CTRL 0x6A              // User control register
+#define FIFO_COUNT_H 0x72           // High bits from FIFO counter
+#define FIFO_COUNT_L 0x73           // Low bits from FIFO counter
+#define FIFO_R_W 0x74               // FIFO Read and Write register
+
 
 //Timer Variables
 volatile int interruptCounter;
@@ -17,41 +32,119 @@ hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 //Timer function
-void IRAM_ATTR onTimer() {
+void IRAM_ATTR onTimer(void) {
   portENTER_CRITICAL_ISR(&timerMux);
   interruptCounter++;
   portEXIT_CRITICAL_ISR(&timerMux);
  
 }
+/*
+int16_t FIFO_count(void){
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(FIFO_COUNT_H);
+  Wire.endTransmission(false);
+  Wire.requestFrom((uint16_t)MPU_ADDR, (int8_t)2, (bool)true);
+  return (int16_t) (Wire.read() << 8 | Wire.read());
+}
 
-//Accelerometer elemet
-Adafruit_MPU6050 mpu;
-
+void FIFO_read(int16_t count, int16_t * values){
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(FIFO_R_W);
+  Wire.endTransmission(false);
+  Wire.requestFrom((uint16_t)MPU_ADDR, (int8_t)count,(bool)true);
+  for(int16_t i=0; i<count; i+=2){
+    values[(i+1)/2] = (int16_t) (Wire.read() << 8 | Wire.read());
+  }
+}
+*/
 //Accelerometer configuration
 void mpu_config(void){
-  mpu.setGyroStandby(true,true,true);           //every gyro axis off
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-  mpu.setAccelerometerStandby(true,true,false); //Only Z accelerometer axis on
-  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
-  //mpu.setTemperatureStandby(true);              //temperature sensor off
+  Wire.begin();
+  Wire.setClock(I2C_CLOCK);
+  Wire.beginTransmission(MPU_ADDR);
+  byte config;
+  
+  config = 0;
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(PWR_MGMT_1);
+  Wire.write(config);
+  Wire.endTransmission(true);
+  delay(100);
+  
+  Wire.beginTransmission(MPU_ADDR);
+  config |= (1 << 7);                         //Reset Device Config (1) reset
+  Wire.write(PWR_MGMT_1);
+  Wire.write(config);
+  Wire.endTransmission(true);
 
-  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
-  mpu.setInterruptPinPolarity(false);           //High when active
-  mpu.enableCycle(false);                       //Don't enable cycle measurements
-  mpu.enableSleep(false);                       //Don't enable sleep
-  //mpu.setSampleRateDivisor();
-  //mpu.setClock();
-  //mpu.setFsyncSampleOutput();
-  //mpu.setInterruptPinLatch();
+  delay(1000);
 
-#ifdef DEBUG
-  Serial.print("Sample Divisor:");
-  Serial.println(mpu.getSampleRateDivisor());
-  Serial.print("Clock:");
-  Serial.println(mpu.getClock());
-  Serial.print("FSync:");
-  Serial.println(mpu.getFsyncSampleOutput());
-#endif
+  config = 0;
+  config &= (0 << 0) && (0 << 1) && (0 << 2); //Clock Selection: (0) Internal
+  config |= (1 << 3);                         //Disable Temp sensor
+  config &= (0 << 4);                         // ----
+  config &= (0 << 5);                         // Cycle: (0) Disabled
+  config &= (0 << 6);                         // Sleep: (0) Wake
+  config &= (0 << 7);                         //Reset Device Config (0) on
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(PWR_MGMT_1);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;
+  config |= (1 << 0);                         // Gyro Z Standby: (1) Standby
+  config |= (1 << 1);                         // Gyro Y Standby: (1) Standby
+  config |= (1 << 2);                         // Gyro X Standby: (1) Standby
+  config &= (0 << 3);                         // Accel Z Standby: (0) Working
+  config |= (1 << 4);                         // Accel Y Standby: (1) Standby
+  config |= (1 << 5);                         // Accel X Standby: (1) Standby
+  config &= (0 << 6) & (0 << 7);              // Low Power Wake Control: (not used)
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(PWR_MGMT_2);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0x08;                              // Sample rate divider: 8
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(SMPRT_DIV);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;                                 // low pass filter 260Hz and Fsync deactivated
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(CONFIG);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;
+  config |= (1 << 0);                         // AFS_SEL = 1 (Scale = +-4g)
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(ACCEL_CONFIG);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;
+  config |= (1 << 3);                         // ACCEL FIFO EN = 1 (FIFO enabled for Accel)
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(FIFO_EN);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;
+  config |= (1 << 0);                         // Data Ready INT: (1) enabled
+  config |= (1 << 4);                         // FIFO OVERFLOW INT: (1) enabled
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(INT_ENABLE);
+  Wire.write(config);
+  Wire.endTransmission(true);
+
+  config = 0;
+  config |= (1 << 2);                         // FIFO reset: (1) reset
+  config |= (1 << 6);                         // FIFO buffer  operations: (1) enabled
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(USER_CTRL);
+  Wire.write(config);
+  Wire.endTransmission(true);
 }
 
 void setup(void) {
@@ -65,14 +158,7 @@ void setup(void) {
   timerAlarmWrite(timer, TIMER_COUNTER_DELAY, TIMER_AUTO_RELOAD);
   timerAlarmEnable(timer);
 
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
+  mpu_config();
 
   delay(100);
 }
@@ -84,13 +170,14 @@ void loop() {
     interruptCounter--;
     portEXIT_CRITICAL(&timerMux);
     totalInterruptCounter++;
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    Serial.print(a.acceleration.z);
+
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(ACCEL_ZOUT_H);  // starting with register 0x3B (ACCEL_ZOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, (uint8_t)2, CONTINUE_COMM);
+    int16_t AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    Serial.print(AcZ);
     Serial.print(",");
-    Serial.print(temp.temperature);
-    Serial.print(",");
-    Serial.print(totalInterruptCounter);
-    Serial.println();
+    Serial.println(totalInterruptCounter);
   }
 }
